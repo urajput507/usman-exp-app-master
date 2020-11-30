067aaf66c90dc6d61ffb14afe91557fe1c2d8b14
Usman Rajput <66204853+urajput507@users.noreply.github.com>
2020-11-30 14:50:13 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+ 
+        <div className="container">
+           <h1>TheQRMart</h1> <br/><br/>
+         <button className="btn">Customer</button> <br/>
+         <button className="btn">Admin</button> <br/>
+        </div>
+
+  );
+}
+
+export default App;
